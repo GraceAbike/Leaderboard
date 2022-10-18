16c5d6647e74f58f3a005113310be4d24f12fb03
Grace Abike <mara234216@gmail.com>
2022-10-19 01:57:24 +0200

    Create renderScores.js

---
+++ src/modules/renderScores.js
@@ -0,0 +1,13 @@
+import scoresArray from './scores.js';
+
+const recentScoresEl = document.querySelector('.recent-scores__scores');
+const renderScores = () => {
+  let markup = '';
+  scoresArray.forEach((item) => {
+    markup += `  <li><span class="recent-scores__name">${item.name}: </span><span class="recent-scores__score">${item.score}</span> </li>`;
+  });
+
+  recentScoresEl.innerHTML = markup;
+};
+
+export default renderScores;
