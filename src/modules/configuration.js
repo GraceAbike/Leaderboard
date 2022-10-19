2bfa3476ed08b641143e1fd299c4ea76e595969a
Grace Abike <mara234216@gmail.com>
2022-10-19 22:55:52 +0200

    Create configuration.js

---
+++ src/modules/configuration.js
@@ -0,0 +1,6 @@
+const gameKey = 'OOyBSMfV40z7yA2OR8pe';
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OOyBSMfV40z7yA2OR8pe/scores';
+
+const apiEndPoint = `${baseURL}/games/${gameKey}/scores`;
+
+export default apiEndPoint;
