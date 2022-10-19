4ef2768e056c22119af1575747a8e570baec517f
Grace Abike <mara234216@gmail.com>
2022-10-19 23:33:06 +0200

    Update configuration.js

--- src/modules/configuration.js
+++ src/modules/configuration.js
@@ -1,5 +1,5 @@
 const gameKey = 'OOyBSMfV40z7yA2OR8pe';
-const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OOyBSMfV40z7yA2OR8pe/scores';
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 
 const apiEndPoint = `${baseURL}/games/${gameKey}/scores`;
 
