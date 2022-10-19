7b7dd062f741092cdcc4ee06b6e248ee0861c8b3
Grace Abike <mara234216@gmail.com>
2022-10-19 22:55:47 +0200

    Update src folders

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,17 +1,39 @@
-import scoresArray from './scores.js';
-import renderScores from './renderScores.js';
+import apiEndPoint from './configuration.js';
 
 const nameEl = document.querySelector('.name');
 const scoreEl = document.querySelector('.score');
 const formEl = document.querySelector('form');
+const addMsgEl = document.querySelector('.add-msg');
 
-const addScore = () => {
-  const name = nameEl.value;
+const addScore = async () => {
+  const user = nameEl.value;
   const score = scoreEl.value;
-  const scoreOBJ = { name, score };
-  scoresArray.push(scoreOBJ);
-  renderScores();
-  formEl.reset();
-};
+  const scoreOBJ = { user, score };
+  if (!user || !score) {
+    addMsgEl.innerHTML = 'Please enter a name and score';
+    return;
+  }
 
+  try {
+    const res = await fetch(apiEndPoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(scoreOBJ),
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      addMsgEl.innerHTML = 'Not Successful';
+    }
+    addMsgEl.innerHTML = data.result;
+    formEl.reset();
+  } catch (err) {
+    addMsgEl.innerHTML = 'Not Successful';
+  }
+
+  setTimeout(() => {
+    addMsgEl.innerHTML = '';
+  }, 2000);
+};
 export default addScore;
