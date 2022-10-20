5ff8a2d8272213e0c34c45924c7a4ea3ea3f3f21
Grace Abike <mara234216@gmail.com>
2022-10-20 11:44:58 +0200

    Update renderScores.js

--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -1,6 +1,7 @@
 import apiEndPoint from './configuration.js';
 
 const recentScoresEl = document.querySelector('.scores-scores');
+const addMsgEl = document.querySelector('.add-msg');
 const renderScores = async () => {
   let markup = '';
 
@@ -10,13 +11,16 @@ const renderScores = async () => {
     if (!res.ok) {
       recentScoresEl.innerHTML = 'Server Down';
     }
-    data.result.forEach((item) => {
-      markup += `  <li><span class="name">${item.user}: </span><span class="scores-score">${item.score}</span> </li>`;
+    data.result.sort((a, b) => b.score - a.score).forEach((item, index) => {
+      markup += `<li class="player-scores flex-row"><span class="player-index flex-row">${index + 1}</span><span class="name">${item.user} </span><span class="scores-score">${item.score}</span></li>`;
     });
     recentScoresEl.innerHTML = markup;
+    addMsgEl.innerHTML = 'Scores Updated Successfully';
+    setTimeout(() => {
+      addMsgEl.innerHTML = '';
+    }, 2000);
   } catch (err) {
     recentScoresEl.innerHTML = 'Server Down';
   }
 };
-
 export default renderScores;
